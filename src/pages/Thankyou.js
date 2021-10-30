0daefd36ddad1f4b824ffa7096d49fc9c121dff6
deepak4455 <curiousdeepak29@gmail.com>
2021-10-30 15:10:42 +0530

    elwen

---
+++ src/pages/Thankyou.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+const Thankyou = () => {
+  return (
+    <Layout>
+      <SEO title="Thank You" />
+      <div className="container pt-16 px-6 pb-14">
+        <h1 class="pb-12">Thank you</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default Thankyou
